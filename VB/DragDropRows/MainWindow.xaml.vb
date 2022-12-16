7f957b6ec7dc0b91d019574e8f49588bbded8ae6
DevExpressExampleBot <fakeEmail>
2022-12-16 16:44:05 +0400

    autogenerate VB

--- VB/DragDropRows/MainWindow.xaml.vb
+++ VB/DragDropRows/MainWindow.xaml.vb
@@ -1,81 +1,67 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Linq
 Imports System.Windows
 Imports DevExpress.Xpf.Grid
 
 Namespace Drag_Drop_rows
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Private Const listSize As Integer = 5
-		Private _ID As Integer = listSize
+    Public Partial Class MainWindow
+        Inherits System.Windows.Window
 
-		Public ReadOnly Property ID() As Integer
-			Get
-				_ID += 1
-'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
-'ORIGINAL LINE: return ++_ID;
-				Return _ID
-			End Get
-		End Property
+        Const listSize As Integer = 5
 
-		Public Sub New()
-			InitializeComponent()
-			TreeListView.DisableOptimizedModeVerification = True
+        Private _ID As Integer = Drag_Drop_rows.MainWindow.listSize
 
-			treeList.ItemsSource = CreateData(listSize)
-			gridControl.ItemsSource = CreateData(listSize)
-		End Sub
+        Public ReadOnly Property ID As Integer
+            Get
+                Return System.Threading.Interlocked.Increment(Me._ID)
+            End Get
+        End Property
 
-		Private Shared Function CreateData(ByVal listCnt As Integer) As List(Of Task)
-			Dim rand As New Random()
-			Dim list As New List(Of Task)()
-			For i As Integer = 0 To listCnt - 1
-				list.Add(New Task With {
-					.ID = i + 1,
-					.ParentID = If(i Mod 2 = 0, 0, i),
-					.Name = "Task" & (i + 1),
-					.IsComplete = Math.Round(rand.NextDouble()) = 0
-				})
-			Next i
-			Return list
-		End Function
+        Public Sub New()
+            Me.InitializeComponent()
+            DevExpress.Xpf.Grid.TreeListView.DisableOptimizedModeVerification = True
+            Me.treeList.ItemsSource = Drag_Drop_rows.MainWindow.CreateData(Drag_Drop_rows.MainWindow.listSize)
+            Me.gridControl.ItemsSource = Drag_Drop_rows.MainWindow.CreateData(Drag_Drop_rows.MainWindow.listSize)
+        End Sub
 
-		Public Class Task
-			Public Property ID() As Integer
-			Public Property ParentID() As Integer
-			Public Property Name() As String
-			Public Property IsComplete() As Boolean
-		End Class
+        Private Shared Function CreateData(ByVal listCnt As Integer) As List(Of Drag_Drop_rows.MainWindow.Task)
+            Dim rand As System.Random = New System.Random()
+            Dim list As System.Collections.Generic.List(Of Drag_Drop_rows.MainWindow.Task) = New System.Collections.Generic.List(Of Drag_Drop_rows.MainWindow.Task)()
+            For i As Integer = 0 To listCnt - 1
+                list.Add(New Drag_Drop_rows.MainWindow.Task With {.ID = i + 1, .ParentID = If(i Mod 2 = 0, 0, i), .Name = "Task" & (i + 1), .IsComplete = System.Math.Round(rand.NextDouble()) = 0})
+            Next
 
-		Private Sub CustomTreeListDragDropManager_Drop(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.DragDrop.TreeListDropEventArgs)
-			If TypeOf e.DraggedRows(0) Is TreeListNode Then
-				Return
-			End If
-			For i As Integer = 0 To e.DraggedRows.Count - 1
-				Dim task As Task = TryCast(e.DraggedRows(i), Task)
-				e.DraggedRows(i) = New Task With {
-					.ID = ID,
-					.ParentID = task.ParentID,
-					.Name = task.Name,
-					.IsComplete = task.IsComplete
-				}
-			Next i
-		End Sub
+            Return list
+        End Function
 
-		Private Sub CustomGridDragDropManager_Drop(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.DragDrop.GridDropEventArgs)
-			If TypeOf e.DraggedRows(0) Is TreeListNode Then
-				For i As Integer = 0 To e.DraggedRows.Count - 1
-					Dim task As Task = TryCast((TryCast(e.DraggedRows(i), TreeListNode)).Content, Task)
-					e.DraggedRows(i) = New Task With {
-						.ID = ID,
-						.ParentID = task.ParentID,
-						.Name = task.Name,
-						.IsComplete = task.IsComplete
-					}
-				Next i
-			End If
-		End Sub
-	End Class
+        Public Class Task
+
+            Public Property ID As Integer
+
+            Public Property ParentID As Integer
+
+            Public Property Name As String
+
+            Public Property IsComplete As Boolean
+        End Class
+
+        Private Sub CustomTreeListDragDropManager_Drop(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.DragDrop.TreeListDropEventArgs)
+            If TypeOf e.DraggedRows(0) Is DevExpress.Xpf.Grid.TreeListNode Then Return
+            For i As Integer = 0 To e.DraggedRows.Count - 1
+                Dim task As Drag_Drop_rows.MainWindow.Task = TryCast(e.DraggedRows(i), Drag_Drop_rows.MainWindow.Task)
+                e.DraggedRows(i) = New Drag_Drop_rows.MainWindow.Task With {.ID = Me.ID, .ParentID = task.ParentID, .Name = task.Name, .IsComplete = task.IsComplete}
+            Next
+        End Sub
+
+        Private Sub CustomGridDragDropManager_Drop(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.DragDrop.GridDropEventArgs)
+            If TypeOf e.DraggedRows(0) Is DevExpress.Xpf.Grid.TreeListNode Then
+                For i As Integer = 0 To e.DraggedRows.Count - 1
+                    Dim task As Drag_Drop_rows.MainWindow.Task = TryCast(TryCast(e.DraggedRows(CInt((i))), DevExpress.Xpf.Grid.TreeListNode).Content, Drag_Drop_rows.MainWindow.Task)
+                    e.DraggedRows(i) = New Drag_Drop_rows.MainWindow.Task With {.ID = Me.ID, .ParentID = task.ParentID, .Name = task.Name, .IsComplete = task.IsComplete}
+                Next
+            End If
+        End Sub
+    End Class
 End Namespace
